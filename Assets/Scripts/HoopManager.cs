d2fad8fe16c807460a714f8f1565572aa0df9ff4
Rohan Venkatapuram <sharpedo75@gmail.com>
2024-03-06 23:48:50 -0800

    adjusted basketball positioning, scale, and which prefab is getting overriden

--- Assets/Scripts/HoopManager.cs
+++ Assets/Scripts/HoopManager.cs
@@ -7,11 +7,14 @@ public class HoopManager : MonoBehaviour
     private IEnumerator CheckHoops() {
         while (true)
         {
+            Debug.Log("Checking for Hoops");
             yield return new WaitForSeconds(1);
             GameObject[] currentHoops = GameObject.FindGameObjectsWithTag("Hoop");
             if (currentHoops.Length > 0) continue;
+            Debug.Log("No Hoops Found");
             HoopSpawner[] allWalls = FindObjectsOfType<HoopSpawner>();
             if (allWalls.Length == 0) continue;
+            Debug.Log("Wall Found");
             HoopSpawner randomWall = allWalls[UnityEngine.Random.Range(0, allWalls.Length - 1)];
             randomWall.SpawnHoop();
         }
